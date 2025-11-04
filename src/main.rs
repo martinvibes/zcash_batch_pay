a45a0005116c5069ada6363e07f58661d3038c05
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-11-04 17:04:42 +0400

    feat: migrate to sqllite, working sync

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::borrow::BorrowMut;
 use std::fmt::Error;
 use std::num::NonZeroU32;
+use std::path::Path;
 use std::process::CommandArgs;
 
 use bip0039::{English, Mnemonic};
+use rand::RngCore;
 use tonic::transport::{Channel, ClientTlsConfig, Endpoint};
 use tonic::{IntoRequest, Response};
 use zcash_client_backend::data_api::wallet::{self, ConfirmationsPolicy};
@@ -15,23 +18,37 @@ use zcash_client_backend::proto::service::{self, BlockId};
 use zcash_client_backend::proto::service::{
     ChainSpec, compact_tx_streamer_client::CompactTxStreamerClient,
 };
-use zcash_client_memory::MemoryWalletDb;
+use zcash_client_backend::sync::run;
+use zcash_client_memory::{MemBlockCache, MemoryWalletDb};
+use zcash_client_sqlite::util::{Clock, SystemClock};
+use zcash_client_sqlite::wallet::init::init_wallet_db;
 use zcash_protocol::consensus::{Parameters, TEST_NETWORK};
 use zip32::AccountId;
 
+use zcash_client_sqlite::{AccountUuid, FsBlockDb, WalletDb};
+
+use rand_core::OsRng;
+use time::OffsetDateTime;
+
 mod client;
 
-pub struct SpendingAccount<P: Parameters> {
-    memory_db: MemoryWalletDb<P>,
+pub struct SpendingAccount<C: BorrowMut<rusqlite::Connection>, P: Parameters, CL: Clock, R: RngCore>
+{
+    db: WalletDb<C, P, CL, R>,
     client: CompactTxStreamerClient<Channel>,
 }
 
-impl<P: Parameters> SpendingAccount<P> {
+impl<C: BorrowMut<rusqlite::Connection>, P: Parameters, CL: Clock, R: RngCore>
+    SpendingAccount<C, P, CL, R>
+{
     pub fn new(
-        memory_db: MemoryWalletDb<P>,
+        memory_db: WalletDb<C, P, CL, R>,
         client: CompactTxStreamerClient<Channel>,
-    ) -> SpendingAccount<P> {
-        SpendingAccount { memory_db, client }
+    ) -> SpendingAccount<C, P, CL, R> {
+        SpendingAccount {
+            db: memory_db,
+            client,
+        }
     }
 
     pub async fn init(
@@ -61,15 +78,36 @@ impl<P: Parameters> SpendingAccount<P> {
                 .expect("birthday tree step failing")
         };
 
-        self.memory_db.import_account_ufvk(
-            "default",
-            &unified_key,
-            &birthday,
-            AccountPurpose::Spending { derivation: None },
-            None,
-        );
+        self.db
+            .import_account_ufvk(
+                "default",
+                &unified_key,
+                &birthday,
+                AccountPurpose::Spending { derivation: None },
+                None,
+            )
+            .unwrap();
 
         Ok(())
+    }
+
+    pub async fn sync(&mut self) {
+        println!("syncing...");
+
+        // TODO: This should be held in the Wallet struct so we can download in parallel
+        let db_cache = MemBlockCache::new();
+
+        run(
+            &mut self.client,
+            &TEST_NETWORK,
+            &db_cache,
+            &mut self.db,
+            100,
+        )
+        .await
+        .unwrap();
+
+        println!("synced");
     }
 
     pub async fn get_latest_block(&mut self) -> Result<BlockId, Box<dyn std::error::Error>> {
@@ -83,11 +121,11 @@ impl<P: Parameters> SpendingAccount<P> {
     pub async fn wallet_summary(
         &mut self,
     ) -> Result<
-        WalletSummary<<MemoryWalletDb<P> as WalletRead>::AccountId>,
+        WalletSummary<<WalletDb<C, P, CL, R> as WalletRead>::AccountId>,
         Box<dyn std::error::Error>,
     > {
         let wallet_summary = self
-            .memory_db
+            .db
             .get_wallet_summary(ConfirmationsPolicy::MIN)?
             .unwrap();
         Ok(wallet_summary)
@@ -98,6 +136,14 @@ impl<P: Parameters> SpendingAccount<P> {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let max_checkpoints = 100;
     let params = TEST_NETWORK;
+
+    // 3. Use SQLite wallet database (fully implemented!)
+    let db_data_path = Path::new("./wallet.db");
+    let mut wallet_db = WalletDb::for_path(db_data_path, params, SystemClock, OsRng)?;
+
+    // 4. Initialize the wallet if it's new
+    // (This creates the database schema)
+    init_wallet_db(&mut wallet_db, None).unwrap();
 
     let memory_wallet = MemoryWalletDb::new(params, max_checkpoints);
 
@@ -113,15 +159,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut test_client = CompactTxStreamerClient::new(channel.clone());
 
-    let mut account = SpendingAccount::new(memory_wallet, test_client);
+    let mut account = SpendingAccount::new(wallet_db, test_client);
     let seed_phrase = "bronze foil box peace chunk use veteran course friend help chuckle ketchup destroy spin village alien embark gospel thank sustain afford hidden shadow suffer";
 
     let latest_block = account.get_latest_block().await?;
 
     println!("latest block: {:?}", latest_block);
 
-    account.init(seed_phrase, latest_block.height).await?;
+    // account.init(seed_phrase, latest_block.height).await?;
     println!("account initialized");
+
+    account.sync().await;
+
+    let wallet_summary = account.wallet_summary().await?;
 
     Ok(())
 }
